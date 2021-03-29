faa61fbd09e016eecd8b19a9e5c6080b3bc7569f
muzili <2483744673@qq.com>
2021-03-29 14:21:41 +0800

    更新初始化CSS样式

--- src/index.js
+++ src/index.js
@@ -12,25 +12,15 @@ import Sider from './pages/slide/slide'
 function Index () { 
   return (
     <div className="body-box">
-
+      
       <header className="heand-title">标题</header>
-
       <div className="content-box">
         <div className="left-box">
           <Sider></Sider>
         </div>
 
         <div className="right-box">
-          <ul className="right-ul">
-            <li className="li-item li-item1">11</li>
-            <li className="li-item li-item2">22</li>
-            <li className="li-item li-item3">33</li>
-            <li className="li-item li-item4">44</li>
-          </ul>
-
-          <div className="right-div">
-            **************
-          </div>
+         {/*  */}
         </div>
       </div>
       
